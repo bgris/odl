9ed587020ba9d6b04f926340ca5a795238f5886a
Barbara Gris <gris@kth.se>
2017-08-08 10:45:07 +0200

    Addition of MRC files in odl/tomo/data that were deleted/modified during rebase

--- odl/tomo/data/__init__.py
+++ odl/tomo/data/__init__.py
@@ -1,10 +1,19 @@
-# Copyright 2014-2017 The ODL contributors
+# Copyright 2014-2016 The ODL development group
 #
 # This file is part of ODL.
 #
-# This Source Code Form is subject to the terms of the Mozilla Public License,
-# v. 2.0. If a copy of the MPL was not distributed with this file, You can
-# obtain one at https://mozilla.org/MPL/2.0/.
+# ODL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ODL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
 """Routines for Data I/O."""
 
@@ -12,3 +21,9 @@
 from __future__ import absolute_import
 
 __all__ = ()
+
+from .uncompr_bin import *
+__all__ += uncompr_bin.__all__
+
+from .mrc import *
+__all__ += mrc.__all__
